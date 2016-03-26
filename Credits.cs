0611d7e5d54e836fa516f3598af19c7fa0f09fa9
John Close <John Close>
2016-03-26 19:40:54 -0400

    Inital Assets Commit

---
+++ Credits.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Credits : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public bool isColliding(float x, float y, float w, float h, float x2, float y2, float w2, float h2){
+		return !(y+h <= y2|| y>= y2+h2 || x+w <= x2|| x >= x2+w2);
+	}
+
+}
